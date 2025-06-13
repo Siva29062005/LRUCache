ee30d7507273872aa52421982b5960661f101eed
Sivakumar N <122144280+Siva29062005@users.noreply.github.com>
2025-06-13 18:26:17 +0530

    Update

--- Node.java
+++ Node.java
@@ -1,25 +1,23 @@
-public class Node {
-    public int key;
-    public int data;
-    public Node prev;
-    public Node next;
+public class Node<K, V> {
+    public K key;
+    public V data;
+    public Node<K, V> prev;
+    public Node<K, V> next;
 
-    public Node(int key,int data, Node prev, Node next) {
+    public Node(K key, V data, Node<K, V> prev, Node<K, V> next) {
         this.key = key;
         this.data = data;
         this.prev = prev;
         this.next = next;
     }
 
-    public Node(int key,int data) {
+    public Node(K key, V data) {
         this.key = key;
         this.data = data;
-        this.prev = null;
-        this.next = null;
     }
 
     public Node() {
-        this.prev=null;
-        this.next=null;
+        this.prev = null;
+        this.next = null;
     }
 }
